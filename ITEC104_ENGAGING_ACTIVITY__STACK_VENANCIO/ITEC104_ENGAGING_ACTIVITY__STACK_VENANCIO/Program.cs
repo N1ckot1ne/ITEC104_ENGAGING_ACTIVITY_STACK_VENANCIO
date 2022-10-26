2a40783339a498c9bf5703f5b5be96dbfe94ec27
Venancio, Nicko A <106007314+N1ckot1ne@users.noreply.github.com>
2022-10-26 23:41:57 +0800

    Add files via upload

---
+++ ITEC104_ENGAGING_ACTIVITY__STACK_VENANCIO/ITEC104_ENGAGING_ACTIVITY__STACK_VENANCIO/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+
+namespace ITEC104_ENGAGING_ACTIVITY__STACK_VENANCIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack st = new Stack();
+
+            st.Push('A');
+            st.Push('M');
+            st.Push('G');
+            st.Push('W');
+
+            Console.WriteLine("Current stack: ");
+
+            foreach (char c in st)
+            {
+                Console.Write(c + " ");
+            }
+
+            st.Push('V');
+            st.Push('H');
+            Console.WriteLine("The next poppable value in stack: {0}", st.Peek());
+            Console.Write("Current stack: ");
+
+            foreach (char c in st)
+            {
+                Console.Write(c + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Removing Values");
+            st.Pop();
+            st.Pop();
+            st.Pop();
+
+            Console.Write("Current stack:");           
+
+            foreach (char c in st)
+            {
+                Console.WriteLine(c + " ");
+            }
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
